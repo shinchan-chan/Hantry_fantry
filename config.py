b83ba1a719a5bb1841d84d477ca2049d6f91c117
shinchan-chan <shinchanadamopyt21@gmail.com>
2025-02-04 08:29:27 +0530

    Add files via upload

--- config.py
+++ config.py
@@ -29,7 +29,7 @@ DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://adamopytbusiness1:uSswEjo
 DB_NAME = os.environ.get("DATABASE_NAME", "Mitsuri_Pro_File_Bot")
 
 #force sub channel id, if you want enable force sub
-FORCESUB_CHANNEL = int(os.environ.get("FORCESUB_CHANNEL", "-1001715114844"))
+FORCESUB_CHANNEL = int(os.environ.get("FORCESUB_CHANNEL", "-1002360962434"))
 FORCESUB_CHANNEL2 = int(os.environ.get("FORCESUB_CHANNEL2", "-1002395070466"))
 
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
