8c29c7db3469717dc92037bd81f3de5e848b3a5a
shinchan-chan <shinchanadamopyt21@gmail.com>
2025-08-02 17:38:05 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -158,7 +158,7 @@ async def short_url(client: Client, message: Message, base64_string):
         buttons = [
             [
                 InlineKeyboardButton(text="Download", url=short_link),
-                InlineKeyboardButton(text="Tutorial", url="https://t.me/+Xi_PbTfFgktjMDk1")
+                InlineKeyboardButton(text="Tutorial", url="https://t.me/+KPJ5glEICD40Njc1")
             ],
             [
                 InlineKeyboardButton(text="Premium", callback_data="premium")
