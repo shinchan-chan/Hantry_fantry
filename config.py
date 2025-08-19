cc1a3b68521ed6f1d1ec8cbb0227e3f58ddfb2d8
shinchan-chan <shinchanadamopyt21@gmail.com>
2025-08-19 15:29:29 +0530

    Update config.py

--- config.py
+++ config.py
@@ -65,8 +65,8 @@ PROTECT_CONTENT = True if os.environ.get('PROTECT_CONTENT', "False") == "True" e
 DISABLE_CHANNEL_BUTTON = os.environ.get("DISABLE_CHANNEL_BUTTON", None) == 'True'
 
 #Short Url or Api
-SHORT_URL = os.environ.get("SHORTNER_URL", "arolinks.com")
-SHORT_API = os.environ.get("SHORTNER_API", "744bdf08ccf566a7d02235bcc380e4ffbe971a90")
+SHORT_URL = os.environ.get("SHORTNER_URL", "iskerts.com")
+SHORT_API = os.environ.get("SHORTNER_API", "b1fd999a61e6098c2b2924dc8c5616e3da28cc35")
 
 BOT_STATS_TEXT = "<b>BOT UPTIME</b>\n{uptime}"
 USER_REPLY_TEXT = "Pʟᴇᴀꜱᴇ ᴅᴏɴ'ᴛ ᴍᴇꜱꜱᴀɢᴇ ᴍᴇ ᴅɪʀᴇᴄᴛʟʏ ɪ ᴀᴍ ᴏɴʟʏ ᴡᴏʀᴋ ꜰᴏʀ - [ @Ecchi_Network ]"
